573ff44136962376680982268aecbfb064eb421b
manu9812 <32127007+manu9812@users.noreply.github.com>
2018-11-18 11:29:08 -0500

    curso c

---
+++ c/Nfor.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+void  main() {
+	int var;
+	for (var=1;var<=10; var++){
+		printf("tu variable vale: %d\n", var);
+	}
+	printf("gracias, adios");
+}
